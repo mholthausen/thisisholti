b60a5335e59c4339e1e4a09edee4c310d4c720fc
Michael Holthausen <mholthausen@terrestris.de>
2022-12-15 16:16:07 +0100

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Michael Holthausen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="normalize.css"></link>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome!" />
-        <p className="description">
-          Something big will come here!
+        <p className="description">Something big will come here!</p>
+        <p>
+          <a href="https://twitter.com/thisisholti" target={'_blank'}>
+            <TwitterIcon fontSize='large'/>
+          </a>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
