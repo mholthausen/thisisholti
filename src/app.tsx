db64749f41048524398b18fdf71a21c04d62a20e
Michael Holthausen <mholthausen@icloud.com>
2023-11-07 21:38:41 +0100

    feat: new content

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import Numbers from './components/Numbers/Numbers';
 
 import '@csstools/normalize.css';
 import './app.scss';
 
 const App = () => {
-  return <Numbers initValue={42} />;
+  return <div>Coming soon...</div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
